a575a9e452032ef8797ca1737901352940a97f6f
Mohosin2126 <mohosin2126@gmail.com>
2024-06-11 10:31:08 +0600

    added button with link

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -7,8 +7,40 @@ import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import Image from 'next/image';
 import logo from "@/assets/logo.webp"
+import { IconButton } from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import Link from 'next/link';
 
-const pages = ['Products', 'Pricing', 'Blog'];
+
+const navItems=[
+
+{
+  route:"Home",
+  pathname:"/",
+},
+{
+  route:"Pages",
+  pathname:"/pages",
+},
+{
+  route:"Category",
+  pathname:"/category",
+},
+{
+  route:"News",
+  pathname:"/news",
+},
+{
+  route:"About",
+  pathname:"/about",
+},
+{
+  route:"Contact",
+  pathname:"/contact",
+},
+  ]
+
+
 const Navbar = () => {
 
 
@@ -18,17 +50,30 @@ const Navbar = () => {
         <Toolbar disableGutters>
           <Image src={logo} width={100} height={100} alt="logo" />
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
+          <Box>
+            {navItems.map((item) => (
+             <Link key={item} href={item.pathname}>
+        <Button className="text-white" >
+          {item.route}
+        </Button>
+             </Link>
             ))}
           </Box>
 
+          <Box>
+            <IconButton>
+              <FacebookIcon></FacebookIcon>
+            </IconButton>
+            <IconButton>
+              <FacebookIcon></FacebookIcon>
+            </IconButton>
+            <IconButton>
+              <FacebookIcon></FacebookIcon>
+            </IconButton>
+            <IconButton>
+              <FacebookIcon></FacebookIcon>
+            </IconButton>
+          </Box>
 
         </Toolbar>
       </Container>
