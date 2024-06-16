e6f36956d359adb77a4fc3fe183200e00346852a
Mohosin212601~ <mohosin2126@gmail.com>
2024-06-16 14:39:06 +0600

    navbar done

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,89 +1,88 @@
-"use client"
+"use client";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+import { IconButton, Stack } from "@mui/material";
 
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import Image from 'next/image';
-import logo from "@/assets/logo.webp"
-import { IconButton } from '@mui/material';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import Link from 'next/link';
+// icons
+import FacebookIcon from "@mui/icons-material/Facebook";
+import Link from "next/link";
 
+const navItems = [
+  {
+    route: "Home",
+    pathname: "/",
+  },
+  {
+    route: "Pages",
+    pathname: "/pages",
+  },
+  {
+    route: "Category",
+    pathname: "/categories/news?category=all-news",
+  },
+  {
+    route: "News",
+    pathname: "/news",
+  },
+  {
+    route: "About",
+    pathname: "/about",
+  },
+  {
+    route: "Contact",
+    pathname: "/contact",
+  },
+];
 
-const navItems=[
-
-{
-  route:"Home",
-  pathname:"/",
-},
-{
-  route:"Pages",
-  pathname:"/pages",
-},
-{
-  route:"Category",
-  pathname:"/category",
-},
-{
-  route:"News",
-  pathname:"/news",
-},
-{
-  route:"About",
-  pathname:"/about",
-},
-{
-  route:"Contact",
-  pathname:"/contact",
-},
-  ]
-
-
-const Navbar = () => {
-
-
+function Navbar() {
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-        <div className='flex items-center justify-between'>
-        <div className='flex  items-center'>
-        <Image src={logo} width={50} height={50} className='border rounded-xl h-14 w-12'  alt="logo" />
+    <>
+      <AppBar position="static" className="bg-black">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Image src={logo} width={100} height={100} alt="logo" />
 
-<Box>
-  {navItems.map((item) => (
-   <Link key={item} href={item.pathname}>
-<Button className="text-white" >
-{item.route}
-</Button>
-   </Link>
-  ))}
-</Box>
-        </div>
+            <Box className="w-full text-center">
+              {navItems.map((item) => (
+                <Link key={item} href={item.pathname}>
+                  <Button className="text-white">{item.route}</Button>
+                </Link>
+              ))}
+            </Box>
 
-        <div>
-        <Box>
-            <IconButton>
-              <FacebookIcon></FacebookIcon>
-            </IconButton>
-            <IconButton>
-              <FacebookIcon></FacebookIcon>
-            </IconButton>
-            <IconButton>
-              <FacebookIcon></FacebookIcon>
-            </IconButton>
-            <IconButton>
-              <FacebookIcon></FacebookIcon>
-            </IconButton>
-          </Box>
-        </div>
-        </div>
-
-        </Toolbar>
-      </Container>
-    </AppBar>
+            <Box>
+              <Stack
+                direction="row"
+                sx={{
+                  "& svg": {
+                    color: "white",
+                  },
+                }}
+              >
+                <IconButton>
+                  <FacebookIcon />
+                </IconButton>
+                <IconButton>
+                  <FacebookIcon />
+                </IconButton>
+                <IconButton>
+                  <FacebookIcon />
+                </IconButton>
+                <IconButton>
+                  <FacebookIcon />
+                </IconButton>
+              </Stack>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </>
   );
 }
 export default Navbar;
