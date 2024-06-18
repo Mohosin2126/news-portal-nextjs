f55c6d14999df4d89200ddcdd0086f9a0588ae9e
Mohosin212601~ <mohosin2126@gmail.com>
2024-06-18 13:28:47 +0600

    take month

--- src/utils/getCurrentDate.js
+++ src/utils/getCurrentDate.js
@@ -8,6 +8,25 @@ export default function getCurrentDate(){
         "Friday",
         "Saturday",
       ];
+
+      const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+    
+      const day = days[currentDate.getDay()];
+      const month = months[currentDate.getMonth()];
+      
     return (
         <h1>hello this is date</h1>
     )
