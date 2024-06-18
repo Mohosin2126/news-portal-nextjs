105d0c13ce23e8aee768073ef88e334d38d95088
Mohosin212601~ <mohosin2126@gmail.com>
2024-06-18 13:27:51 +0600

    take days

--- src/utils/getCurrentDate.js
+++ src/utils/getCurrentDate.js
@@ -1,5 +1,13 @@
 export default function getCurrentDate(){
-
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
     return (
         <h1>hello this is date</h1>
     )
