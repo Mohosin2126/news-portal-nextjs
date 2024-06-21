9ad3f02c84edba2ba8dfb0d57fd6587b7367b2af
Mohosin212601~ <mohosin2126@gmail.com>
2024-06-21 20:45:22 +0600

    imported necessary things from material ui

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,16 @@ import LatestNews from "@/components/latest-news";
 import SideBar from "@/components/sidebar";
 import { Grid } from "@mui/material";
 
-export default function Home(){
+export default function Home() {
 
   return (
     <Grid container spacing={2}>
-    <Grid item xs={8}>
-<LatestNews></LatestNews>
+      <Grid item xs={8}>
+        <LatestNews></LatestNews>
+      </Grid>
+      <Grid item xs={4}>
+        <SideBar></SideBar>
+      </Grid>
     </Grid>
-    <Grid item xs={4}>
-<SideBar></SideBar>
-    </Grid>
-  </Grid>
   )
 }
--- src/components/latest-news/index.js
+++ src/components/latest-news/index.js
@@ -1,6 +1,30 @@
+import {
+    Box,
+    Card,
+    CardActionArea,
+    CardContent,
+    CardMedia,
+    Grid,
+    Typography,
+  } from "@mui/material";
+import Image from "next/image";
+
 export default function LatestNews(){
 
     return (
-        <h1>hello this is me latest news</h1>
+        <Box className="my-5">
+      <Card>
+      
+      </Card>
+
+      <Grid
+        className="mt-5"
+        container
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+      >
+
+      </Grid>
+    </Box>
     )
 }
