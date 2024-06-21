b5e36ae893c5b211ff417761608bd7ed055cbbf3
Mohosin212601~ <mohosin2126@gmail.com>
2024-06-21 20:50:48 +0600

    added top news photo in home

--- src/components/latest-news/index.js
+++ src/components/latest-news/index.js
@@ -8,13 +8,19 @@ import {
     Typography,
   } from "@mui/material";
 import Image from "next/image";
-
+import topNews from "../../assets/top-news.png"
 export default function LatestNews(){
 
     return (
         <Box className="my-5">
       <Card>
-      
+      <CardActionArea>
+          <CardMedia>
+            <Image src={topNews} alt="top news"/>
+           
+          </CardMedia>
+        
+        </CardActionArea>
       </Card>
 
       <Grid
