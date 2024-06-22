d0ec40e46c00b03950a3f7487a12503361bc6dba
Mohosin212601~ <mohosin2126@gmail.com>
2024-06-22 12:11:42 +0600

    added grid with four card

--- src/components/latest-news/index.js
+++ src/components/latest-news/index.js
@@ -48,14 +48,152 @@ export default function LatestNews(){
         </CardActionArea>
       </Card>
 
-      <Grid
-        className="mt-5"
-        container
-        rowSpacing={1}
-        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-      >
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+  <Grid item xs={6}>
+  <Card>
+      <CardActionArea>
+          <CardMedia>
+            <Image src={topNews} height={500} width={800} alt="top news"/>
+           
+          </CardMedia>
+          <CardContent>
+            <p
+              className="
+                      w-[100px]
+                      bg-red-500
+                      px-2
+                      text-white
+                      my-5
+                      rounded
+                      "
+            >
+          category
+            </p>
+            <Typography gutterBottom >
+            Bitcoin climbs as Elon Musk says tesla likely to accept it again 
+            </Typography>
+            <Typography gutterBottom className="my-3">
+         By Mohosin 
+            </Typography>
 
-      </Grid>
+            <Typography variant="body2" color="text.secondary">
+            It is a long established fact that a reader will be
+                    distracted by the readable content of a page when looking at
+                    its layout........
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+  </Grid>
+  <Grid item xs={6}>
+  <Card>
+      <CardActionArea>
+          <CardMedia>
+            <Image src={topNews} height={500} width={800} alt="top news"/>
+           
+          </CardMedia>
+          <CardContent>
+            <p
+              className="
+                      w-[100px]
+                      bg-red-500
+                      px-2
+                      text-white
+                      my-5
+                      rounded
+                      "
+            >
+          category
+            </p>
+            <Typography gutterBottom >
+            Bitcoin climbs as Elon Musk says tesla likely to accept it again 
+            </Typography>
+            <Typography gutterBottom className="my-3">
+         By Mohosin 
+            </Typography>
+
+            <Typography variant="body2" color="text.secondary">
+            It is a long established fact that a reader will be
+                    distracted by the readable content of a page when looking at
+                    its layout........
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+  </Grid>
+  <Grid item xs={6}>
+  <Card>
+      <CardActionArea>
+          <CardMedia>
+            <Image src={topNews} height={500} width={800} alt="top news"/>
+           
+          </CardMedia>
+          <CardContent>
+            <p
+              className="
+                      w-[100px]
+                      bg-red-500
+                      px-2
+                      text-white
+                      my-5
+                      rounded
+                      "
+            >
+          category
+            </p>
+            <Typography gutterBottom >
+            Bitcoin climbs as Elon Musk says tesla likely to accept it again 
+            </Typography>
+            <Typography gutterBottom className="my-3">
+         By Mohosin 
+            </Typography>
+
+            <Typography variant="body2" color="text.secondary">
+            It is a long established fact that a reader will be
+                    distracted by the readable content of a page when looking at
+                    its layout........
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+  </Grid>
+  <Grid item xs={6}>
+  <Card>
+      <CardActionArea>
+          <CardMedia>
+            <Image src={topNews} height={500} width={800} alt="top news"/>
+           
+          </CardMedia>
+          <CardContent>
+            <p
+              className="
+                      w-[100px]
+                      bg-red-500
+                      px-2
+                      text-white
+                      my-5
+                      rounded
+                      "
+            >
+          category
+            </p>
+            <Typography gutterBottom >
+            Bitcoin climbs as Elon Musk says tesla likely to accept it again 
+            </Typography>
+            <Typography gutterBottom className="my-3">
+         By Mohosin 
+            </Typography>
+
+            <Typography variant="body2" color="text.secondary">
+            It is a long established fact that a reader will be
+                    distracted by the readable content of a page when looking at
+                    its layout........
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+  </Grid>
+</Grid>
     </Box>
     )
 }
