2cb6415b809b6f0afdd1155bb9b61d29e70820be
Mohosin212601~ <mohosin2126@gmail.com>
2024-06-22 12:26:58 +0600

    added sidebar and edit layout with container

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared";
+import { Container } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,10 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar></Navbar>
-        {children}
-        <Footer></Footer>
-        </body>
+        <Navbar />
+        <Container className="min-h-screen">{children}</Container>
+        <Footer />
+      </body>
     </html>
   );
 }
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,6 +1,45 @@
+import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
+import Image from "next/image";
+import sidebarNews from "../../assets/side-top-news.png"
+
 export default function SideBar(){
 
     return(
-        <h1>hello this is me sidebar</h1>
+       <Box className="my-5">
+<Card>
+      <CardActionArea>
+          <CardMedia>
+            <Image src={sidebarNews} height={500}  alt="top news"/>
+           
+          </CardMedia>
+          <CardContent>
+            <p
+              className="
+                      w-[100px]
+                      bg-red-500
+                      px-2
+                      text-white
+                      my-5
+                      rounded
+                      "
+            >
+          category
+            </p>
+            <Typography gutterBottom variant="h5" component="div">
+            Bitcoin climbs as Elon Musk says tesla likely to accept it again 
+            </Typography>
+            <Typography gutterBottom className="my-3">
+         By Mohosin 
+            </Typography>
+
+            <Typography variant="body2" color="text.secondary">
+            It is a long established fact that a reader will be
+                    distracted by the readable content of a page when looking at
+                    its layout........
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+       </Box>
     )
 }
